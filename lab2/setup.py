ffd6888e14a644d296f6b62dc21c3a9d305fd92a
Niyas-A <niyasttshr33@gmail.com>
2024-05-30 16:36:38 -0700

    create gui_service and gui_client

--- lab2/setup.py
+++ lab2/setup.py
@@ -20,8 +20,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': ['turtle_sensor = lab2.turtle_sensor:main',
-		            'service = lab2.service_go_pupper:main',
+		                    'service = lab2.service_go_pupper:main',
                             'client = lab2.client_go_pupper:main',
+                            'gui_service = lab2.gui_pupper_service:main',
+                            'gui_client = lab2.gui_pupper_client:main',
         ],
     },
 )
